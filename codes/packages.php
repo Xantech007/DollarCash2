e94cdfdb9f1afc3006faeb787e05447b56b89734
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-04 04:09:01 +0200

    Update packages.php

--- codes/packages.php
+++ codes/packages.php
@@ -5,13 +5,11 @@ include('../config/dbcon.php');
 if(isset($_POST['add_package']))
 {
     $name = mysqli_real_escape_string($con,$_POST['name']);
-    $duration = mysqli_real_escape_string($con,$_POST['duration']);
-    $percent = mysqli_real_escape_string($con,$_POST['percent']);
-    $min = mysqli_real_escape_string($con,$_POST['min_amount']);
+
     $max = mysqli_real_escape_string($con,$_POST['max_amount']);
     $status = $_POST['status'] == true ? '1' : '0';
 
-    $query = "INSERT INTO packages(name,duration,percent,min_a,max_a,status) VALUES ('$name','$duration','$percent','$min','$max','$status')";
+    $query = "INSERT INTO packages(name,max_a,status) VALUES ('$name','$max','$status')";
     $query_run = mysqli_query($con ,$query);
 
     if($query_run)
@@ -26,13 +24,11 @@ if(isset($_POST['edit_package']))
 {
     $id = mysqli_real_escape_string($con,$_POST['edit_package']);
     $name = mysqli_real_escape_string($con,$_POST['name']);
-    $duration = mysqli_real_escape_string($con,$_POST['duration']);
-    $percent = mysqli_real_escape_string($con,$_POST['percent']);
-    $min = mysqli_real_escape_string($con,$_POST['min_amount']);
+    
     $max = mysqli_real_escape_string($con,$_POST['max_amount']);
     $status = $_POST['status'] == true ? '1' : '0';
 
-    $query_u = "UPDATE packages SET name='$name',duration='$duration',percent='$percent',min_a='$min',max_a='$max',status='$status' WHERE id='$id'";
+    $query_u = "UPDATE packages SET name='$name',max_a='$max',status='$status' WHERE id='$id'";
     $query_u_run = mysqli_query($con,$query_u);
 
     if($query_u_run)
