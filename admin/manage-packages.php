51ec2c671f30edb2ab232c5c90b0cbb6df46e3c9
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-04 06:29:55 +0200

    Update manage-packages.php

--- admin/manage-packages.php
+++ admin/manage-packages.php
@@ -5,93 +5,85 @@ include('inc/navbar.php');
 include('inc/sidebar.php');
 ?>
 
- 
+<main id="main" class="main">
+    <div class="pagetitle">
+        <h1>Manage Packages</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+                <li class="breadcrumb-item">Users</li>
+                <li class="breadcrumb-item active">packages</li>
+            </ol>     
+        </nav>     
+    </div>
 
-  <!-- ======= Sidebar ======= -->
-
-
-  <main id="main" class="main">
- 
-  <div class="pagetitle">
-      <h1>Manage Packages</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
-          <li class="breadcrumb-item">Users</li>
-          <li class="breadcrumb-item active">packages</li>
-        </ol>     
-      </nav>     
-    </div><!-- End Page Title -->  
     <style>
-        .add-btn{
-            display:flex;
-            justify-content:center;
-            align-items:center;
-            margin:15px 0
+        .add-btn {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 15px 0
         }
     </style>
-         <div class="add-btn">
-           <a href="add-package" class="btn btn-secondary float-center">Add New Package</a>
-        </div>
+
+    <div class="add-btn">
+        <a href="add-package" class="btn btn-secondary float-center">Add New Package</a>
+    </div>
 
     <div class="container text-center">
-        <div class="row">
-           
-                   
-                        <?php 
-                        $query = "SELECT * FROM packages ORDER BY created_at DESC";
-                        $query_run = mysqli_query($con, $query);
-                        if($query_run)
-                        {
-                            foreach($query_run as $data)
-                            { ?>
-                             <div class="col-md-4">
-                    <div class="card text-center">
-                        <div class="card-header">
-                            <?= $data['name'] ?>                      
-                        </div>
-                             <div class="card-body mt-2">
-                            
-                            
-                            <div class="mt-3">
-                                                       
-                                <h6>Amount: $<?= $data['max_a'] ?> </h6>
+        <?php
+        // First, get distinct cashtags
+        $cashtag_query = "SELECT DISTINCT cashtag FROM packages ORDER BY cashtag";
+        $cashtag_result = mysqli_query($con, $cashtag_query);
+
+        if ($cashtag_result && mysqli_num_rows($cashtag_result) > 0) {
+            while ($cashtag_row = mysqli_fetch_assoc($cashtag_result)) {
+                $current_cashtag = $cashtag_row['cashtag'];
+        ?>
+                <h3 class="mt-4"><?php echo htmlspecialchars($current_cashtag); ?></h3>
+                <div class="row">
+                    <?php
+                    // Query packages for current cashtag
+                    $query = "SELECT * FROM packages WHERE cashtag = '" . mysqli_real_escape_string($con, $current_cashtag) . "' ORDER BY created_at DESC";
+                    $query_run = mysqli_query($con, $query);
+                    
+                    if ($query_run && mysqli_num_rows($query_run) > 0) {
+                        while ($data = mysqli_fetch_assoc($query_run)) {
+                    ?>
+                            <div class="col-md-4">
+                                <div class="card text-center">
+                                    <div class="card-header">
+                                        <?php echo htmlspecialchars($data['name']); ?>                      
+                                    </div>
+                                    <div class="card-body mt-2">
+                                        <div class="mt-3">
+                                            <h6>CashTag: <?php echo htmlspecialchars($data['cashtag']); ?></h6>
+                                            <h6>Amount: $<?php echo htmlspecialchars($data['max_a']); ?></h6>
+                                        </div>
+                                        <div class="mt-3">                          
+                                            <form action="../codes/packages.php" method="POST">
+                                                <a href="edit-package?id=<?php echo $data['id']; ?>" class="btn btn-outline-secondary mt-3">Edit Package</a> 
+                                                <button class="btn btn-outline-danger mt-3" name="delete" value="<?php echo $data['id']; ?>">Delete Package</button>
+                                            </form>                               
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mt-3">                          
-                                <form action="../codes/packages.php"method="POST">
-                                <a href="edit-package?id=<?=$data['id']?>" class="btn btn-outline-secondary mt-3">Edit Package</a> 
-                                <button class="btn btn-outline-danger mt-3"name="delete"value="<?= $data['id'] ?>">Delete Package</button>
-                                </form>                               
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                           <?php }
+                    <?php
                         }
-                        ?>
-                        
-                                              
-              
-          
-           
-        </div>    
-   </div>       
-      
-              
-       
+                    } else {
+                        echo '<p>No packages found for this CashTag.</p>';
+                    }
+                    ?>
+                </div>
+        <?php
+            }
+        } else {
+            echo '<p>No packages found.</p>';
+        }
+        ?>
+    </div>       
+</main>
 
-       
- 
-  
-            
-
-        
-   
-
-    
-
-  </main><!-- End #main -->
- 
 <?php include('inc/footer.php'); ?>
-
-</html>
+         </html>
