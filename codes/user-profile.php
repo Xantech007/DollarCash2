e1173f19be4329ef6d48406fef2e322a56691b8f
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-04 12:03:36 +0200

    Update user-profile.php

--- codes/user-profile.php
+++ codes/user-profile.php
@@ -9,7 +9,6 @@ if(isset($_POST['update-profile']))
     $country = mysqli_real_escape_string($con, $_POST['country']);   
     $address = mysqli_real_escape_string($con, $_POST['address']);
     $email = mysqli_real_escape_string($con, $_POST['email']);
-    $btc_wallet = mysqli_real_escape_string($con, $_POST['btc_wallet']);
 
     $old_filename = $_POST['old_image'];
 
@@ -36,7 +35,7 @@ if(isset($_POST['update-profile']))
                 //new image name    
                 $update_filename = $filename;   
 
-                $query = "UPDATE users SET name='$name',country='$country',address='$address',email='$email',btc_wallet='$btc_wallet',image='$update_filename' WHERE email='$email' LIMIT 1";
+                $query = "UPDATE users SET name='$name',country='$country',address='$address',email='$email',image='$update_filename' WHERE email='$email' LIMIT 1";
                 $query_run = mysqli_query($con, $query);
 
                 if($query_run)
@@ -74,7 +73,7 @@ if(isset($_POST['update-profile']))
     {  
         //old image name
         $update_filename = $old_filename;
-        $query = "UPDATE users SET name='$name',country='$country',address='$address',email='$email',btc_wallet='$btc_wallet',image='$update_filename' WHERE email='$email' LIMIT 1";
+        $query = "UPDATE users SET name='$name',country='$country',address='$address',email='$email',image='$update_filename' WHERE email='$email' LIMIT 1";
         $query_run = mysqli_query($con, $query);
         if($query_run)
         {
