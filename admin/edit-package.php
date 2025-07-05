619db8bab33be3ec446d0f9d1cef8481b51ae9df
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-05 14:13:39 +0200

    Update edit-package.php

--- admin/edit-package.php
+++ admin/edit-package.php
@@ -76,7 +76,7 @@ include('inc/sidebar.php');
                             <input name="cashtag" type="text" class="form-control" id="cashtag" required value="<?php echo htmlspecialchars($cashtag); ?>">
                         </div>
                         <div class="col-md-6 form-group mb-3">
-                            <label for="max_amount" class="mb-2">Max Amount</label>
+                            <label for="max_amount" class="mb-2">Amount</label>
                             <input name="max_amount" type="number" class="form-control" id="max_amount" required value="<?php echo htmlspecialchars($max_amount); ?>">
                         </div> 
                         <div class="col-md-6 form-group mb-3">
