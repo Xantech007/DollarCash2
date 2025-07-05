782d1213239b6d2eab7815fb17e032d4977a7ba2
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-05 15:18:07 +0200

    Update withdrawals.php

--- codes/withdrawals.php
+++ codes/withdrawals.php
@@ -90,7 +90,7 @@ if (isset($_POST['withdraw'])) {
         $update_stmt->bind_param("ds", $new_balance, $email);
 
         if ($update_stmt->execute()) {
-            $_SESSION['success'] = "$currency$total Payment Sent to $momo_name";
+            $_SESSION['success'] = "$currency$total Sent to $momo_name";
             header("Location: ../users/withdrawals");
             exit(0);
         } else {
