9de69d48810aab64af4926ad726882bd5314e314
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-10 05:12:28 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<div class="mainmenu-area"style="background:linear-gradient(to bottom,#008ee0,#F5F5F5);border-bottom:none">
+<div class="mainmenu-area"style="background:linear-gradient(to bottom,#008ee0,#00cae0);border-bottom:none">
 		<div class="container">
 			<div class="row">
 				
