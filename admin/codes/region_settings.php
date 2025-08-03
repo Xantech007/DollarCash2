a67205de709d4720496b3f0bf512cc28c742bff8
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-08-03 07:23:43 +0200

    Update region_settings.php

--- admin/codes/region_settings.php
+++ admin/codes/region_settings.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include('../config/dbcon.php');
-include('inc/countries.php');
+include('../../config/dbcon.php');
+include('../inc/countries.php');
 
 if (!isset($_SESSION['id'])) {
     $_SESSION['error'] = "Please log in to access this page.";
